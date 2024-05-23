39c4b519e4b6ac5922c912475f116deaa7b25598
Bryson Sullivan-Farrell <44358513+brysonsf@users.noreply.github.com>
2024-05-23 14:42:58 +0000

    added ping command and tutorial setup for bot

---
+++ commands/utility/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// placed inside module.exports so they can be read by other files; namely the command loader and command deployment scripts
+module.exports = {
+	data: new SlashCommandBuilder() 
+	// provides the command definition for registering to Discord.
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) { 
+		// functionality to run from our event handler when the command is used.
+		await interaction.reply('Pong!');
+	},
+};
