e9692a09f700f23dc4bb93085580cd0b73f1987c
Bryson Sullivan-Farrell <44358513+brysonsf@users.noreply.github.com>
2024-06-04 17:49:18 +0000

    added color identity and images

--- commands/commander/commander.js
+++ commands/commander/commander.js
@@ -1,5 +1,5 @@
 // colors: W(white) R(red) B(black) U(blue) G(green) 
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, blockQuote, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,8 +13,8 @@ module.exports = {
         const commanderName = interaction.options.getString('name');
         const edhCommander = commanderName.replace(/\s/g,"-");
 		let linkToEDH = 'https://json.edhrec.com/pages/commanders/' + edhCommander + '.json';
+		let edhCard = 'https://edhrec.com/commanders/' + edhCommander;
         const edhRecData = await (await fetch(linkToEDH)).json();
-        let themes = [];
         /*
         edhRecData.panels.links.forEach(category =>{
             if(category.header==="Themes"){
@@ -27,15 +27,57 @@ module.exports = {
         if(edhRecData.code){
             await interaction.reply({content:'Commander ' + commanderName + ' cannot be found.'});
         }else{
+            let themes = [];
             edhRecData.panels.tribelinks.themes.forEach(theme =>{
                 themes.push({themeName: theme.value, themeCount: theme.count});
             });
-            console.log(themes);
             let strBuilt = '';
             themes.forEach(theme =>{
                 strBuilt+= theme.themeName + ' ' + theme.themeCount + '\n';
             });
-            await interaction.reply({content:'Searching Commander details: ' + commanderName + '...\nThemes:\n' + strBuilt });
+            const highlighted = blockQuote(strBuilt);
+
+            let cardInfo;
+            cardInfo = edhRecData.container.json_dict.card;
+            const colorIdentities = {
+                'U': 'Blue',
+                'G': 'Green',
+                'R': 'Red',
+                'W': 'White',
+                'B': 'Black',
+            }
+            let embedColour = [];
+            console.log(cardInfo);
+            cardInfo.color_identity.forEach(color=>{
+                console.log(colorIdentities[color]);
+                // red green blue white red
+                embedColour.push(colorIdentities[color]);
+            });
+            const exampleEmbed = new EmbedBuilder()
+                .setColor('Grey')
+                .setTitle(commanderName)
+                .setURL(edhCard)
+                .addFields(
+                    { name: 'Themes', value: highlighted},
+                )
+                .setImage(cardInfo.image_uris[0].normal) // first one is the full card
+                .setFooter({ text: commanderName, iconURL: cardInfo.image_uris[0].art_crop }); // the second one is just center image
+            if (embedColour.length===1){
+                exampleEmbed
+                    .setColor(embedColour[0]);
+            }else{
+                let x = Math.ceil(Math.random() * 5); // max color identity is 5 (not counting colorless) pick one at random
+                console.log(x);
+                if(x>embedColour.length-1){
+                    x = 0; // take first color if we go outside of colors
+                }
+                exampleEmbed
+                    .setColor(embedColour[x]);
+            }
+            
+
+            await interaction.reply({content:'Searching Commander details: ' + commanderName + '...',  
+            embeds: [exampleEmbed]});
         }
         
 	},
